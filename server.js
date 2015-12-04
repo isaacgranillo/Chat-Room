9a99a7e52a69cd3504d135e2feb0edc58243c3fc
Isaac Granillo <isaac.granillo@gmail.com>
2015-12-04 14:04:00 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname+'/static'));
+var server = app.listen(1337);
+var io = require('socket.io').listen(server);
+var users = [];
+
+io.sockets.on('connection', function(socket){
+	console.log("Socket Connected");
+
+	socket.on('newUser', function(data){
+		users.push({ name: data.name, socket_id: socket.id 
+		})
+	io.emit('updateUserList', users);	
+	});
+
+	socket.on('disconnect', function(){
+		console.log("Socket Disconnected");
+		for(index in users){
+			if (users[index].socket_id == socket.id){
+				users.splice(index, 1);
+				io.emit('updateUserList', users)
+				break;
+			}
+		}
+	})
+
+	socket.on('newMessage', function(data){
+		for (index in users) {
+			if(users[index].socket_id == socket.id) {
+				users[index].name
+				data.message
+				io.emit('addMessage', { name: users[index].name, message: data.message })
+				break;
+			}
+		}
+	})
+
+
+});
